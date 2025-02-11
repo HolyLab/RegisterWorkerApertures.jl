42f1adee68b467a7b1d37f5e91449ee765b18d46
Dae Woo Kim <kim503@wustl.edu>
2025-02-11 11:20:37 -0600

    Version 0.2.3

--- src/RegisterWorkerApertures.jl
+++ src/RegisterWorkerApertures.jl
@@ -28,7 +28,7 @@ end
 
 function load_mm_package(dev)
     if dev >= 0
-        eval(:(using CUDAdrv, CUDAnative, CuArrays, RegisterMismatchCuda))
+        eval(:(using CUDA, RegisterMismatchCuda))
     else
         eval(:(using RegisterMismatch))
     end
@@ -38,14 +38,21 @@ end
 function init!(algorithm::Apertures)
     if algorithm.dev >= 0
         cuda_init!(algorithm)
+        CUDA.allowscalar()
     end
     nothing
 end
 
 function cuda_init!(algorithm)
     dev = CuDevice(algorithm.dev)
-    global old_active_context = CuCurrentContext()
-    if old_active_context == nothing || device(old_active_context) != dev
+    global old_active_context
+    try
+        old_active_context = current_context()
+        if old_active_context == nothing || device(old_active_context) != dev
+            device!(dev)
+        end
+    catch e
+        old_active_context = nothing
         device!(dev)
     end
     fixed = algorithm.fixed
